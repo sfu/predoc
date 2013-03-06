c7b72f003715eb355b3cc1ba9e6936e3bb15a795
Andrew Leung <andrewleung@sfu.ca>
2013-03-06 10:00:25 -0800

    Initial import (rails new)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctor::Application.config.secret_token = 'e9aecc45c9d0e0faa1742117e92f9d10df66e79627453de5c23a5fcf728f1e404ae11d1e14439ddd72e5a8fc4fd627ca4f1091f1993bec51666cb00a2ede54e3'
